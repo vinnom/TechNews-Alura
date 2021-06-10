71a559a6d21eb72598740ca743583d7d6f063d87
vinnom <vinnom.2d@gmail.com>
2021-06-10 00:50:13 -0300

    movendo o conteúdo visual para um fragment utilizando xml

--- app/src/main/java/br/com/alura/technews/ui/activity/ListaNoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/ListaNoticiasActivity.kt
@@ -3,31 +3,22 @@ package br.com.alura.technews.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import br.com.alura.technews.R
 import br.com.alura.technews.database.AppDatabase
 import br.com.alura.technews.model.Noticia
 import br.com.alura.technews.repository.NoticiaRepository
-import br.com.alura.technews.repository.Resource
-import br.com.alura.technews.ui.activity.extensions.mostraErro
-import br.com.alura.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
+import br.com.alura.technews.ui.fragment.ListaNoticiasFragment
 import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
 import br.com.alura.technews.ui.viewmodel.NoticiaViewModelFactory
-import kotlinx.android.synthetic.main.activity_lista_noticias.*
 
 private const val TITULO_APPBAR = "Notícias"
-private const val MENSAGEM_FALHA_CARREGAR_NOTICIAS = "Não foi possível carregar as novas notícias"
 
 class ListaNoticiasActivity : AppCompatActivity() {
 
-    private val adapter by lazy {
-        ListaNoticiasAdapter(context = this)
-    }
-
-    private val viewModel by lazy {
+    private val viewModel: ListaNoticiasViewModel by lazy {
         val repository = NoticiaRepository(AppDatabase.getInstance(this).noticiaDAO)
         val factory = NoticiaViewModelFactory(
             ListaNoticiasViewModel::class.java as Class<ViewModel>,
@@ -40,34 +31,19 @@ class ListaNoticiasActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_noticias)
         title = TITULO_APPBAR
-        configuraRecyclerView()
-        configuraFabAdicionaNoticia()
-        buscaNoticias()
     }
 
-    private fun configuraFabAdicionaNoticia() {
-        activity_lista_noticias_fab_salva_noticia.setOnClickListener {
-            abreFormularioModoCriacao()
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+
+        if (fragment is ListaNoticiasFragment) {
+            fragment.fabClicado = { abreFormularioModoCriacao() }
+            fragment.noticiaClicada = { noticia ->
+                abreVisualizadorNoticia(noticia)
+            }
         }
     }
 
-    private fun configuraRecyclerView() {
-        val divisor = DividerItemDecoration(this, VERTICAL)
-        activity_lista_noticias_recyclerview.addItemDecoration(divisor)
-        activity_lista_noticias_recyclerview.adapter = adapter
-        configuraAdapter()
-    }
-
-    private fun configuraAdapter() {
-        adapter.quandoItemClicado = this::abreVisualizadorNoticia
-    }
-
-    private fun buscaNoticias() {
-        viewModel.buscaTodos().observe(this, { resource: Resource<List<Noticia>?> ->
-            resource.dado?.let { adapter.atualiza(resource.dado) }
-            resource.erro?.let { mostraErro(MENSAGEM_FALHA_CARREGAR_NOTICIAS) }
-        })
-    }
 
     private fun abreFormularioModoCriacao() {
         val intent = Intent(this, FormularioNoticiaActivity::class.java)
---
+++ app/src/main/java/br/com/alura/technews/ui/fragment/ListaNoticiasFragment.kt
@@ -0,0 +1,96 @@
+package br.com.alura.technews.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.alura.technews.R
+import br.com.alura.technews.database.AppDatabase
+import br.com.alura.technews.model.Noticia
+import br.com.alura.technews.repository.NoticiaRepository
+import br.com.alura.technews.repository.Resource
+import br.com.alura.technews.ui.fragment.extensions.mostraErro
+import br.com.alura.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
+import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
+import br.com.alura.technews.ui.viewmodel.NoticiaViewModelFactory
+import kotlinx.android.synthetic.main.lista_noticias.*
+
+private const val MENSAGEM_FALHA_CARREGAR_NOTICIAS = "Não foi possível carregar as novas notícias"
+
+class ListaNoticiasFragment : Fragment() {
+
+    lateinit var fabClicado: () -> Unit
+    lateinit var noticiaClicada: (noticia: Noticia) -> Unit
+
+    private val adapter: ListaNoticiasAdapter by lazy {
+        context?.let { contexto ->
+            ListaNoticiasAdapter(context = contexto)
+        } ?: throw IllegalArgumentException("Contexto inválido")
+
+    }
+
+    private val viewModel: ListaNoticiasViewModel by lazy {
+        if (context != null) {
+            val repository = NoticiaRepository(AppDatabase.getInstance(context!!).noticiaDAO)
+            val factory = NoticiaViewModelFactory(
+                ListaNoticiasViewModel::class.java as Class<ViewModel>,
+                repository
+            )
+            ViewModelProvider(
+                context as AppCompatActivity,
+                factory
+            ).get(ListaNoticiasViewModel::class.java)
+        } else {
+            throw IllegalArgumentException("Contexto inválido")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        buscaNoticias()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.lista_noticias, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configuraRecyclerView()
+        configuraFabAdicionaNoticia()
+    }
+
+    private fun configuraFabAdicionaNoticia() {
+        lista_noticias_fab_salva_noticia.setOnClickListener {
+            fabClicado()
+        }
+    }
+
+    private fun configuraRecyclerView() {
+        val divisor = DividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+        lista_noticias_recyclerview.addItemDecoration(divisor)
+        lista_noticias_recyclerview.adapter = adapter
+        configuraAdapter()
+    }
+
+    private fun configuraAdapter() {
+        adapter.quandoItemClicado = noticiaClicada
+    }
+
+    private fun buscaNoticias() {
+        viewModel.buscaTodos().observe(this, { resource: Resource<List<Noticia>?> ->
+            resource.dado?.let { adapter.atualiza(resource.dado) }
+            resource.erro?.let { mostraErro(MENSAGEM_FALHA_CARREGAR_NOTICIAS) }
+        })
+    }
+}
